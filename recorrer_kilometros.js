8038b232dbfa8ff273a447e60493625c05dbd329
Mario Alberto Campo Londoño <marioalbertocampolondono@gmail.com>
2018-01-17 18:43:47 -0500

    Recorrer kilometros
    Generación de distancia recorrida en kms según un día de la semana

---
+++ recorrer_kilometros.js
@@ -0,0 +1,17 @@
+const nombre = 'Mario'
+const dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes']
+
+function correr() {
+    const min = 5
+    const max = 15
+    return Math.round(Math.random() * (max - min)) + min
+}
+let totalKms = 0
+const length = dias.length
+for (let i = 0; i < length; i++) {
+    const kms = correr()
+    totalKms += kms
+    console.log(`El dia ${dias[i]} ${nombre} corrió ${kms}kms`)
+}
+const promedioKms = totalKms / length
+console.log(`En promedio ${nombre} corrió ${promedioKms.toFixed(2)}kms`)
