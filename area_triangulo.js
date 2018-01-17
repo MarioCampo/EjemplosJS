b74e3731d2aae8b413e61b76d011bf1d11d89ca0
Mario Alberto Campo Londoño <marioalbertocampolondono@gmail.com>
2018-01-17 12:00:02 -0500

    Área de un triángulo
    Área de un triángulo con arrow functions.

---
+++ area_triangulo.js
@@ -0,0 +1,4 @@
+let base = 5;
+let altura = 7;
+const areaTriangulo = (base, altura) => base * altura / 2;
+console.log(`El area de un triangulo de base ${base} y altura ${altura} es: ${areaTriangulo(base, altura)}`);
